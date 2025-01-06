a8243ba4ea14bc6e36effee7ceb0175ec4fcfb49
Dhurga <dhurgadharani3@gmail.com>
2025-01-06 22:48:59 +0800

    File Handling

--- user_system.c
+++ user_system.c
@@ -12,6 +12,35 @@ struct account
     int account_num;
 };
 
+void file_handling(int* account_number, struct account* p)
+{
+    int acc_num = *account_number;
+    char file_name[256];
+    sprintf(file_name, "%d", acc_num); //Converts int to str
+
+    char filepath[100] = "users/"; //filepath
+    strcat(filepath, file_name);
+
+    const char ender[] = ".txt"; //const
+    strcat(filepath, ender); //concatenate
+
+
+    FILE* fp = fopen(filepath, "w");
+
+    if(fp==NULL)
+    {
+        printf("\nFile failed to open. Nothing was saved.\n");
+        printf("\nReturning to homescreen.\n\n");
+        return;
+    }
+
+    fprintf(fp, "Name: %s\nPassword: %s\nAccount Number: %d\n", p->name, p->passw, p->account_num);   
+    fclose(fp); 
+    
+ 
+}
+
+
 bool password_checker(char* password)
 {
     int num_count=0;
@@ -64,23 +93,28 @@ void create_acc()
     printf("Enter your name:\n");
     scanf("%s", temp_name);
 
-
-    for(int i=0; i < strlen(temp_name); i++)
+    if (strlen(temp_name) <= 100)
     {
-        if (!isalpha(temp_name[i]))
+        for(int i=0; i < strlen(temp_name); i++)
         {
-            printf("Not valid name. Please enter characters only. Returning to starting screen.\n\n");
-            return;
+            if (!isalpha(temp_name[i]))
+            {
+                printf("Not valid name. Please enter characters only. Returning to starting screen.\n\n");
+                return;
+            }
         }
+    }else
+    {
+        printf("Name cannot be more than 100 characters. Returning to home screen\n\n");
+        return;
     }
-
 
     printf("Enter your password:\n");
     scanf("%s", temp_pass);
 
-    if (strlen(temp_pass) < 8)
+    if (strlen(temp_pass) < 8 || strlen(temp_pass) > 20)
     {
-        printf("Password should not be less than 8 characters. Returning to starting screen.\n\n");
+        printf("Password should not be less than 8 characters or more than 20 characters. Returning to starting screen.\n\n");
         return;
     }
 
@@ -119,7 +153,12 @@ void create_acc()
     strncpy(p.passw, temp_pass, sizeof(p.passw)-1);
     p.account_num = temp_accNum;
 
-    printf("\nName: %s\nPassword:%s\nAccount Number: %d\n\n", p.name, p.passw, p.account_num);
+    printf("\nName: %s\nPassword: %s\nAccount Number: %d\n\n", p.name, p.passw, p.account_num);
+    printf("Please remember your password.\n\n");
+
+    file_handling(&p.account_num, &p);
+
+    return;
 }
 
 void starting_screen(bool* exit_program)
