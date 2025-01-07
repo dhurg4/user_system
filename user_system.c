51eafd0f96c4b2f992b5e73c70363d87107970fc
Dhurga <dhurgadharani3@gmail.com>
2025-01-07 21:04:30 +0800

    Log in features added

--- user_system.c
+++ user_system.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <ctype.h>
 #include <dirent.h>
+#include <time.h>
 
 
 struct account 
@@ -32,9 +33,10 @@ int file_exists(char* filename)
     while (entry != NULL)
     {
         if (strcmp((entry->d_name), ".")!= 0 || strcmp((entry->d_name), "..")!=0)
-            if(strcmp((entry->d_name), *filename))
+        {
+            if(strcmp((entry->d_name), filename))
             {
-                printf("\nFile exists.\n\n");
+                //printf("\nFile exists.\n\n");
                 return 0;
             }
             else
@@ -42,9 +44,11 @@ int file_exists(char* filename)
                 printf("\nFile does not exist.\n\n");
                 return -1;
             }
+        }
     }
     closedir(dp);
-
+    printf("\nFile does not exist.\n\n");
+    return -1;
 }
 
 void file_handling(int* account_number, struct account* p)
@@ -60,11 +64,12 @@ void file_handling(int* account_number, struct account* p)
     strcat(filepath, ender); //concatenate
 
 
-    int exists = file_exists(&file_name);
+    int exists = file_exists(file_name);
 
     switch (exists)
     {
         case 1:
+            //Error opening directory
             return;
         case 0:
             printf("\nAn account with this account number exists. Please log in.\n\n");
@@ -92,24 +97,115 @@ void log_in()
     int account_number;
 
     printf("\nPlease enter your account number here: \n");
-    scanf("%d", account_number);
+    scanf("%d", &account_number);
 
     char file_name[256];
     sprintf(file_name, "%d", account_number);
-    int exists = file_exists(&file_name);
+    
+    char filepath[100] = "users/"; //filepath
+    strcat(filepath, file_name); //concatenate
+
+    const char ender[] = ".txt"; //const
+    strcat(filepath, ender); //concatenate
+
+    int exists = file_exists(filepath);
 
     switch (exists)
     {
         case 1:
+            //Error opening directory
             return;
         case 0:
-            printf("\nAccount found.\n\n");
+            //printf("\nAccount found.\n\n");
             break;
         case -1:
             printf("\nAccount does not exist. Returning to starting screen where you may create an account or exit the program.\n\n");
             return;
     }
 
+    FILE* fp = fopen(filepath, "r");
+
+    if (fp==NULL)
+    {
+        printf("\nError opening file. Returning to homescreen.\n\n");
+        return;
+    }
+
+    char file_data[50];
+
+    int count = 0;
+    char password_data[50];
+
+    while (fgets(file_data, 50, fp)!=NULL) //50 is the approx. length of each line
+    {
+        if (count==1) //Getting the data from the password line
+        {
+            strcpy(password_data, file_data);
+            break;
+        }
+        else
+        {
+            count++;
+        }
+    }
+
+    if (strcmp(password_data, "")==0) //Making sure the line in the file wasn't empty
+    {
+        printf("\nPassword data cannot be found. Returning to home screen.\n\n");
+        return;
+    }
+
+    char password [20];
+    int passw = 10; //Length of the prefix password in the file
+    int pass_len = 20; //Maximum length of the user's pasword
+
+    strncpy(password, password_data + passw, pass_len); //Getting the user's password
+    int user_pass_length = strlen(password);
+    password[user_pass_length-1]='\0';
+
+    char user_password[20];
+
+    int tries=0;
+
+    while (tries < 3)
+    {
+        printf("\nTries left: %d\nPlease enter your password: \n", (3-tries));
+        scanf("%s", user_password);
+        int result = strcmp(password, user_password);
+
+        if (result!=0)
+        {
+            tries++;
+        }
+        else if (result==0)
+        {
+            printf("\nLog in success!\n\n");
+            rewind(fp);
+            while ((fgets(file_data, 256, fp))!=NULL)
+            {
+                printf("%s", file_data);
+            }
+            return;
+        }
+    }
+
+    fclose(fp);
+
+    printf("\nNo more tries. Wait 10 seconds before you can try again.\n\n");
+
+    int seconds = 10;
+
+    while (seconds > 0)
+    {
+        printf("%d...\n", seconds);
+
+        clock_t timer = clock() + CLOCKS_PER_SEC;
+        while (clock() < timer) { }
+
+        seconds--;
+    }
+    
+    printf("\nReturning to starting screen now.\n\n");
 
     return;
 }
@@ -238,9 +334,11 @@ void starting_screen(bool* exit_program)
 {
     int choice;
 
+    printf("---------------------------\n\n     Starting Screen\n\n");
     printf("1. Create Account.\n");
     printf("2. Log Into Account.\n");
     printf("3. Exit program.\n");
+    printf("---------------------------\n");
     scanf("%d", &choice);
 
     switch (choice)
@@ -249,7 +347,7 @@ void starting_screen(bool* exit_program)
             create_acc();
             return;
         case 2:
-            //log_in();
+            log_in();
             return;
         case 3:
             *exit_program = true;
